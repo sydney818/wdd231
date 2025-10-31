cf5889131e43d2246758b5cd332d1a75adaa0844
sydney818 <sydney818@gmail.com>
2025-10-31 14:41:31 -0600

    Initial website commit

---
+++ scripts/navigation.js
@@ -0,0 +1,21 @@
+// Store the selected elements that we are going to use. 
+const navbuttom = document.querySelector('#ham-btn')
+const navlinks = document.querySelector('#nav-bar')
+
+//Toggle the show class off and on
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+})
+
+const navBar = document.querySelector('#nav-bar');
+
+
+
+// Display current year
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// Display the last modified date 
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+
