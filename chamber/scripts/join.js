819030fc1743e682698d0fb3b24c522eab74d7db
sydney818 <sydney818@gmail.com>
2025-11-19 23:54:19 -0700

    fixes to the join page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,43 @@
-document.getElementById("timestamp").value = new Date().toISOString();
+// TIMESTAMP  //
+const timestampField = document.getElementById("time-stamp");
+if (timestampField) {
+    timestampField.value = new Date().toISOString();
+}
+
+
+// THANK YOU HTML //
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    document.getElementById("fname").textContent = urlParams.get("fname");
+    document.getElementById("lname").textContent = urlParams.get("lname");
+    document.getElementById("email").textContent = urlParams.get("email");
+    document.getElementById("phone").textContent = urlParams.get("phone");
+    document.getElementById("organization").textContent = urlParams.get("organization");
+    document.getElementById("timestamp").textContent = urlParams.get("time-stamp");
+
+});
+
+
+// OPEN MODAL //
+
+document.querySelectorAll(".open-modal").forEach(button => {
+    button.addEventListener("click", () => {
+        const modalId = button.parentElement.dataset.modal;
+        const modal = document.getElementById(modalId);
+        modal.showModal();
+    });
+});
+
+document.querySelectorAll(".close").forEach(closeBtn => {
+    closeBtn.addEventListener("click", (e) => {
+        const dialog = e.target.closest("dialog");
+        dialog.close();
+    });
+});
+
+
+
+
