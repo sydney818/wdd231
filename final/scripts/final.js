9c60bc5ec86d6ba222fd1f4f1897e34edcb718bc
sydney818 <sydney818@gmail.com>
2025-12-03 00:36:46 -0700

    updates to final project

--- final/scripts/final.js
+++
@@ -1,8 +0,0 @@
-
-
-// Current Year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Last Modified Date
-document.getElementById("lastModified").textContent =
-    "Last Modified: " + document.lastModified;
---
+++ final/scripts/global.js
@@ -0,0 +1,20 @@
+
+
+// Current Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last Modified Date
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
+
+// HAMBURGER BUTTON
+const navbuttom = document.querySelector('#ham-btn')
+const navlinks = document.querySelector('#nav-bar')
+
+//Toggle the show class off and on
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+})
+
+const navBar = document.querySelector('#nav-bar');
