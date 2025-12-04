75cef0285a7b82f56ab3ee76bab8f9f93bb3f7d6
sydney818 <sydney818@gmail.com>
2025-12-04 00:40:26 -0700

    working on final project

--- final/scripts/global.js
+++ final/scripts/global.js
@@ -18,3 +18,20 @@ navbuttom.addEventListener('click', () => {
 })
 
 const navBar = document.querySelector('#nav-bar');
+
+
+//FORM-RESULTS PAGE
+
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+
+    document.getElementById("out-fname").textContent = params.get("fname");
+    document.getElementById("out-lname").textContent = params.get("lname");
+    document.getElementById("out-parent").textContent = params.get("parent");
+    document.getElementById("out-email").textContent = params.get("email");
+    document.getElementById("out-phone").textContent = params.get("phone");
+    document.getElementById("out-grade").textContent = params.get("grade");
+    document.getElementById("out-exp").textContent = params.get("experience");
+    document.getElementById("out-comments").textContent = params.get("comments") || "None";
+
+})
