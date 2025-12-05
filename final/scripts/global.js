a0b48ba5a5caf368cfe9c11a3c66b8ccce35a43d
sydney818 <sydney818@gmail.com>
2025-12-05 01:19:57 -0700

    updated responsive issue on index page

--- final/scripts/global.js
+++ final/scripts/global.js
@@ -7,17 +7,21 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent =
     "Last Modified: " + document.lastModified;
 
-// HAMBURGER BUTTON
-const navbuttom = document.querySelector('#ham-btn')
-const navlinks = document.querySelector('#nav-bar')
 
-//Toggle the show class off and on
-navbuttom.addEventListener('click', () => {
-    navbuttom.classList.toggle('show');
-    navlinks.classList.toggle('show');
-})
+// ---------------------
+// MOBILE NAV TOGGLE
+// ---------------------
+document.addEventListener("DOMContentLoaded", () => {
 
-const navBar = document.querySelector('#nav-bar');
+    const hamBtn = document.getElementById("ham-btn");
+    const nav = document.getElementById("nav-bar");
+
+    hamBtn.addEventListener("click", () => {
+        hamBtn.classList.toggle("show");
+        nav.classList.toggle("show");
+    });
+
+});
 
 
 
